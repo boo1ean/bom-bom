2872955ffb2a64b56a3f3204c0d72cf40d57ad7a
kirmir <kirmir@outlook.com>
2012-09-29 16:59:10 +0300

    SocketClient.

---
+++ wp7/AirHockey.Client.WinPhone/AirHockey.Client.WinPhone/Network/SocketClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace AirHockey.Client.WinPhone.Network
+{
+    public class SocketClient
+    {
+        readonly Socket _socket;
+
+        private const int ConnectTimeout = 5000;
+
+        public delegate void ConnectCompletedEventHandler();
+        public delegate void ConnectFailedEventHandler(SocketError result);
+        public event ConnectCompletedEventHandler ConnectCompleted;
+        public event ConnectFailedEventHandler ConnectFailed;
+
+        public SocketClient()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Connect(string hostName, int portNumber)
+        {
+            var endPoint = new DnsEndPoint(hostName, portNumber);
+            var socketEventArg = new SocketAsyncEventArgs { RemoteEndPoint = endPoint };
+
+            var subject = new Subject<int>();
+            socketEventArg.Completed += (s, e) => subject.OnNext(0);
+
+            var timeout = subject.Timeout(TimeSpan.FromSeconds(ConnectTimeout));
+            timeout.Subscribe(i => invokeConnectCompleted(), ex => invokeConnectFailed(socketEventArg.SocketError));
+
+            _socket.ConnectAsync(socketEventArg);
+        }
+
+        public void Send(string data)
+        {
+            
+        }
+
+        private void invokeConnectCompleted()
+        {
+            var handler = ConnectCompleted;
+            if (handler != null) handler();
+        }
+
+        private void invokeConnectFailed(SocketError result)
+        {
+            var handler = ConnectFailed;
+            if (handler != null) handler(result);
+        }
+    }
+}
