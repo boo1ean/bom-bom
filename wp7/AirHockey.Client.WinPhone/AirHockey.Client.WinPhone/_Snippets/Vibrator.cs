cbd552c05e70746fdc637f1ae1ebccec7101dc3f
kirmir <kirmir@outlook.com>
2012-09-29 15:09:26 +0300

    Client draft.

---
+++ wp7/AirHockey.Client.WinPhone/AirHockey.Client.WinPhone/_Snippets/Vibrator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Devices;
+
+namespace AirHockey.Client.WinPhone._Snippets
+{
+    public class Vibrator
+    {
+        public static void Vibrate()
+        {
+            VibrateController.Default.Start(TimeSpan.FromMilliseconds(500));
+        }
+    }
+}
