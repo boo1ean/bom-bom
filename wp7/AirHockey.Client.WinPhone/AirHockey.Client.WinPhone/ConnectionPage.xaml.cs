4d9571ec21987cba0936768767cee4f731ab8dd6
Ivan Zaporozhchenko <1van1111@mail.ru>
2012-09-29 16:39:59 +0300

    Add progress bar when click find server and connect

--- wp7/AirHockey.Client.WinPhone/AirHockey.Client.WinPhone/ConnectionPage.xaml.cs
+++ wp7/AirHockey.Client.WinPhone/AirHockey.Client.WinPhone/ConnectionPage.xaml.cs
@@ -10,20 +10,47 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 
 namespace AirHockey.Client.WinPhone
 {
     public partial class ConnectionPage : PhoneApplicationPage
     {
+        private ProgressIndicator indicator=new ProgressIndicator()
+        {
+            IsVisible = false,
+            IsIndeterminate = true,
+            Text = ""
+        };
+
         // Constructor
         public ConnectionPage()
         {
             InitializeComponent();
+            SystemTray.SetProgressIndicator(this, indicator);
         }
 
         private void StartGame(object sender, EventArgs e)
         {
             this.NavigationService.Navigate(new Uri("/Accelerometr.xaml", UriKind.Relative)); 
+        }
+
+        private void ApplicationBarFindButton_Click(object sender, EventArgs e)
+        {
+            indicator.IsVisible = true;
+            
+            //some method to find server
+            MessageText.Text = "Server Finded!";
+            //indicator.IsVisible = false;
+
+        }
+
+        private void ApplicationBarConnectButton_Click(object sender, EventArgs e)
+        {
+            //indicator.IsVisible = true;
+            MessageText.Text = "Connect succesful. You can start the game!";
+            //some method to connect to server
+            indicator.IsVisible = false;
         }
     }
 }
